97a84a75a5419df375bbddbd721341bfbea5e345
Oskar <case112@users.noreply.github.com>
2020-10-10 15:31:51 +0300

    Basic function functionality done

--- convert_to_pdf.py
+++ convert_to_pdf.py
@@ -13,23 +13,26 @@ extensions = [
 
 extension = extensions[0]
 
-selected_directory = os.getcwd() + os.sep
+selected_directory = os.getcwd()
 
 print(selected_directory)
 
+print(os.path.dirname(selected_directory))
+
+pdf_path_name = os.path.dirname(selected_directory) + os.sep + os.path.basename(selected_directory) + '.pdf'
 
 
 def convert():
     pages = []
     for file_in_dir in os.listdir(selected_directory):
         if file_in_dir.endswith(extension):
-            file_path = selected_directory + file_in_dir
+            file_path = selected_directory + os.sep + file_in_dir
             page = Image.open(file_path)
-            print(page)
-            print(file_path)
+            #print(page)
+            #print(file_path)
             pages.append(page.convert("RGB"))
     print("Generating PDF")
-    pages[0].save("out.pdf", save_all = True, append_images=pages[1:])
+    pages[0].save(pdf_path_name, save_all = True, append_images=pages[1:])
 
 
 convert()
