cf35ed06e5d958a7597590ff83e817cf8ac1a810
Oskar <case112@users.noreply.github.com>
2020-10-10 15:12:41 +0300

    Updated convert_to_pdf.py, get file with os instead of glob

--- convert_to_pdf.py
+++ convert_to_pdf.py
@@ -1,10 +1,35 @@
 from PIL import Image
-from PIL import ImageSequence
 import glob
+import tkinter as tk
+import os
 
-pages = []
-for infile in glob.glob("*.tif"):
-    page = Image.open(infile)
-    pages.append(page.convert("RGB"))
-print("opened")
-pages[0].save("out.pdf", save_all = True, append_images=pages[1:])
+
+extensions = [
+    '.tif',
+    '*.tiff',
+    '*.jpg',
+    '*.jpeg'
+]
+
+extension = extensions[0]
+
+selected_directory = os.getcwd() + os.sep
+
+print(selected_directory)
+
+
+
+def convert():
+    pages = []
+    for file_in_dir in os.listdir(selected_directory):
+        if file_in_dir.endswith(extension):
+            file_path = selected_directory + file_in_dir
+            page = Image.open(file_path)
+            print(page)
+            print(file_path)
+            pages.append(page.convert("RGB"))
+    print("Generating PDF")
+    pages[0].save("out.pdf", save_all = True, append_images=pages[1:])
+
+
+convert()
--- test2.py
+++ test2.py
@@ -1,10 +1,24 @@
-from PIL import Image
-from PIL import ImageSequence
-import glob
+import PySimpleGUI as sg
 
-pages = []
-for infile in glob.glob("*.tif"):
-    page = Image.open(infile)
-    pages.append(page.convert("RGB"))
-print("opened")
-pages[0].save("out.pdf", save_all = True, append_images=pages[1:])
+# Define the window's contents
+layout = [[sg.Text("What's your name?")],
+          [sg.Input(key='-INPUT-')],
+          [sg.Text(size=(40,1), key='-OUTPUT-')],
+          [sg.Button('Ok'), sg.Button('Quit')]]
+
+# Create the window
+window = sg.Window('Window Title', layout)
+
+# Display and interact with the Window using an Event Loop
+while True:
+    event, values = window.read()
+
+    
+    # See if user wants to quit or window was closed
+    if event == sg.WINDOW_CLOSED or event == 'Quit':
+        break
+    # Output a message to the window
+    window['-OUTPUT-'].update('Hello ' + values['-INPUT-'] + "! Thanks for trying PySimpleGUI")
+
+# Finish up by removing from the screen
+window.close()
