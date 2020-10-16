02e0540c373d8119cad91400121ca94b28b15c34
Oskar <case112@users.noreply.github.com>
2020-10-16 21:53:26 +0300

    Added multiple directory feature, needs some more debugging

--- multiple_dirs_to_pdf.py
+++ multiple_dirs_to_pdf.py
@@ -21,7 +21,7 @@ layout = [
         sg.Text('Select file extension to convert:')
     ],
     [
-        sg.Checkbox(extensions[0], key=extensions[0]),
+        sg.Checkbox(extensions[0], key=extensions[0], default=True),
         sg.Checkbox(extensions[1], key=extensions[1]),
         sg.Checkbox(extensions[2], key=extensions[2]),
         sg.Checkbox(extensions[3], key=extensions[3]),
@@ -46,26 +46,40 @@ layout = [
         sg.Button('Exit the program', button_color=('white', '#d73a49'))
     ]      
 ]
+
+#os.chdir("/path/to/change/to")
      
 def convert():
     global pages
     global file_problem
     pages = []
-    for file_in_dir in os.listdir(selected_directory):
-        if file_in_dir.lower().endswith(extension):
-            file_path = selected_directory + os.sep + file_in_dir
-            try:
-                page = Image.open(file_path)
-                pages.append(page.convert("RGB"))
-                page.close()
-            except:
+    subfolders = [ f.path for f in os.scandir(selected_directory) if f.is_dir() ]
+
+    if subfolders != []:
+
+        for subfolder in subfolders:
+
+            for file_in_dir in os.listdir(subfolder):
+                if file_in_dir.lower().endswith(extension):
+                    file_path = subfolder + os.sep + file_in_dir
+                    try:
+                        page = Image.open(file_path)
+                        pages.append(page.convert("RGB"))
+                        page.close()
+                    except:
+                        pages = []
+                        file_problem = True
+                        return sg.Popup('There is a problem with "' + file_in_dir + '", it is not possible to convert this file. Aborting. Fix this and please try again.', title='Warning!')  
+
+                    pdf_path_name = os.path.dirname(subfolder) + os.sep + os.path.basename(subfolder) + '.pdf'
+
+            if pages != []:
+                print("Generating PDF " + pdf_path_name)
+                pages[0].save(pdf_path_name, save_all = True, append_images=pages[1:])
                 pages = []
-                file_problem = True
-                return sg.Popup('There is a problem with "' + file_in_dir + '", it is not possible to convert this file. Aborting. Fix this and please try again.', title='Warning!')
-                
-    if pages != []:
-        print("Generating PDF")
-        pages[0].save(pdf_path_name, save_all = True, append_images=pages[1:])
+
+    else:
+        print('no folders found')
 
 window = sg.Window('Convert to PDF', layout)
 
@@ -90,18 +104,17 @@ while True:
         window.FindElement('convert_btn').Update(disabled=False)
         selected_directory = values['user_input_path']
         window.Element('Browse').InitialFolder = selected_directory
-        pdf_path_name = os.path.dirname(selected_directory) + os.sep + os.path.basename(selected_directory) + '.pdf'
 
     if event == 'convert_btn':
         if extension != ():
             file_problem = False
             convert()
             if len(pages) > 1:
-                sg.Popup('Converted ' + str(len(pages)) + ' images to location: ' + pdf_path_name, title='Done!')
-            if file_problem == False and pages == []:
-                for x in range(len(extension)):
-                    ext += str(extension[x]) + ' '
-                sg.Popup('No " ' + ext +'" files found!', title='Error!')
+                sg.Popup('Converted ' + str(len(pages)) + ' images to location: ' + selected_directory, title='Done!')
+            #if file_problem == False and pages == []:
+                #for x in range(len(extension)):
+                    #ext += str(extension[x]) + ' '
+                #sg.Popup('No " ' + ext +'" files found!', title='Error!')
         else:
             sg.Popup('Please select file extension first!', title='Error!')
 
